d17dc38cca75498ccd85ec059ca2d1b13e8713ac
Feng Qi <qi000026@algonquinlive.com>
2024-01-14 00:07:04 -0500

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Feng's Android Labs"
+include(":app")
