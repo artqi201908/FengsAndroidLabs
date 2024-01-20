306e670967a12ddbd770e8b344c461146aaa64fe
Feng Qi <qi000026@algonquinlive.com>
2024-01-20 17:19:38 -0500

    Commit 4 Added extra lines

--- app/src/main/java/algonquin/cst2335/fengqi/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/fengqi/MainActivity.java
@@ -12,3 +12,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+Today is a snowday
+    add test1
+    add test2
+    add test3
+    add test4
+    add test5
+    add test6
+    add test7
+    add test8
+    add test9
+    add test10
