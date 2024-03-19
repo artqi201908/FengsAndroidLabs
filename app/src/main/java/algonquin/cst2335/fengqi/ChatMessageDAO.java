d707d90e82d74ff4143394e420112fbc94b41983
Feng Qi <qi000026@algonquinlive.com>
2024-03-19 15:06:46 -0400

    Week7 finish

--- app/src/main/java/algonquin/cst2335/fengqi/ChatMessageDAO.java
+++ app/src/main/java/algonquin/cst2335/fengqi/ChatMessageDAO.java
@@ -1,21 +1,21 @@
 package algonquin.cst2335.fengqi;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.PrimaryKey;
+import androidx.room.Delete;
 import androidx.room.Query;
-
 import java.util.List;
 
 @Dao
 public interface ChatMessageDAO {
     @Insert
-    void insertMessage(ChatMessage m);
+    void insertMessage(ChatMessage message);
 
     @Query("SELECT * FROM ChatMessage")
     List<ChatMessage> getAllMessages();
 
     @Delete
-    void deleteMessage(ChatMessage m);
+    void deleteMessage(ChatMessage message);
 }
+
+
--- app/src/main/java/algonquin/cst2335/fengqi/ChatRoomViewModel.java
+++ app/src/main/java/algonquin/cst2335/fengqi/ChatRoomViewModel.java
@@ -1,11 +1,12 @@
 package algonquin.cst2335.fengqi;
 
+import java.util.ArrayList;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 
 public class ChatRoomViewModel extends ViewModel {
-    public MutableLiveData<ArrayList<ChatMessage>> chatMessages = new MutableLiveData<>();
-    public MutableLiveData<ArrayList<ChatMessage>> getChatMessages(){return chatMessages;}
+    public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData<>();
 }
+
+
